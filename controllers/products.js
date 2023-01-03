157a205548334892641da66ef92ffccca38e02ff
alifshah <alifhasanshah@gmail.com>
2023-01-03 14:33:10 +0600

    show 10 products

--- controllers/products.js
+++ controllers/products.js
@@ -23,14 +23,13 @@ const getAllProducts = async (req, res) => {
   }
 
   let page = Number(req.query.page) || 1;
-  let limit = Number(req.query.limit) || 8;
+  let limit = Number(req.query.limit) || 10;
 
   let skip = (page -1) *  limit;
   apiData = apiData.skip(skip).limit(limit);
 
-  console.log(queryObject);
-  const myData = await apiData;
-  res.status(200).json({ myData, nbHints: myData.length });
+  const Products = await apiData;
+  res.status(200).json({ Products, nbHints: Products.length });
 };
 const getAllProductsTesting = async (req, res) => {
   const myData = await Product.find(req.query);
