f95659e55e1139e850ff1b4eb93892522e327ede
alifshah <alifhasanshah@gmail.com>
2023-01-03 14:30:16 +0600

    add search filter select field pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,29 +1,44 @@
-const Product = require('../model/product')
+const Product = require("../model/product");
 
 const getAllProducts = async (req, res) => {
-    const {company,name, featured} = req.query;
-    const queryObject = {}
-    if(company){
-        queryObject.company= company;
-        console.log(queryObject.company);
-    }
-    if(featured){
-        queryObject.featured = featured
-    }
-    if(name){
-        queryObject.name = {$regex:name, $options: 'i'}
-    }
-    console.log(queryObject)
-    const myData = await Product.find(queryObject)
-    res.status(200).json({myData})
-}
+  const { company, name, featured, select } = req.query;
+  const queryObject = {};
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (featured) {
+    queryObject.featured = featured;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let apiData = Product.find(queryObject);
+
+  if (select) {
+    let selectFix = select.split(",").join(" ");
+    apiData = apiData.select(selectFix);
+  }
+
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 8;
+
+  let skip = (page -1) *  limit;
+  apiData = apiData.skip(skip).limit(limit);
+
+  console.log(queryObject);
+  const myData = await apiData;
+  res.status(200).json({ myData, nbHints: myData.length });
+};
 const getAllProductsTesting = async (req, res) => {
-    const myData = await Product.find(req.query)
-    console.log(req.query)
-    res.status(200).json({myData})
-}
+  const myData = await Product.find(req.query);
+  console.log(req.query);
+  res.status(200).json({ myData });
+};
 
 module.exports = {
-    getAllProducts,
-    getAllProductsTesting
-}
+  getAllProducts,
+  getAllProductsTesting,
+};
