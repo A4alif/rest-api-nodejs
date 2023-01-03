7e09acd60f26babc779c32ba2b21ae66a868c358
alifshah <alifhasanshah@gmail.com>
2023-01-03 13:14:02 +0600

    advanced search added

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,25 @@
 const Product = require('../model/product')
 
-
 const getAllProducts = async (req, res) => {
-    const myData = await Product.find({})
+    const {company,name, featured} = req.query;
+    const queryObject = {}
+    if(company){
+        queryObject.company= company;
+        console.log(queryObject.company);
+    }
+    if(featured){
+        queryObject.featured = featured
+    }
+    if(name){
+        queryObject.name = {$regex:name, $options: 'i'}
+    }
+    console.log(queryObject)
+    const myData = await Product.find(queryObject)
     res.status(200).json({myData})
 }
 const getAllProductsTesting = async (req, res) => {
-    const myData = await Product.find({})
+    const myData = await Product.find(req.query)
+    console.log(req.query)
     res.status(200).json({myData})
 }
 
